189bc4cf3fae708b8c4c5cfb8cb4d34faabf7726
Malinhara <dinuka.20191070@iit.a.c.lk>
2022-03-19 09:42:14 +0530

    initial commit

---
+++ src/main/java/org/econation/HomeServlet.java
@@ -0,0 +1,19 @@
+package org.econation;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("Admin Login/html");
+        PrintWriter out = resp.getWriter();
+
+    }
+}
+
